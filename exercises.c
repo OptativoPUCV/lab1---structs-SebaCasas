3b8b35a778ca3df3a81b86302a93c0e0f6a861b3
Your Name <you@example.com>
2024-03-14 22:10:35 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -98,7 +98,7 @@ int checkSorted(int arr[], int size) {
     else if(arr[i] >= arr[i+1])
       descendente++;
   }
-  printf("%d", ascendente);
+  //printf("%d", ascendente);
   if(ascendente == size - 1)
     return 1;
   else if(descendente == size - 1)
