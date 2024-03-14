89ae5af5d20f434bc26742ee98565f6826f7a362
Your Name <you@example.com>
2024-03-14 15:01:55 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -41,7 +41,7 @@ newsize apunta a una dirección válida que no ha sido inicializada con nigún v
 */
 int *filterEvenNumbers(int arr[], int size, int *newSize) {
   
-  int arrPares[];
+  int *arrPares[];
   newSize = 0;
   
   for ( int i = 0 ; i < size; i++){
