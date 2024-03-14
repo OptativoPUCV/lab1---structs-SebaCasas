bb43cdb7912e0500d30bcccfd6407cbb2c1b8bd0
Your Name <you@example.com>
2024-03-14 15:01:46 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -39,7 +39,18 @@ los números pares del arreglo original.
 newsize apunta a una dirección válida que no ha sido inicializada con nigún valor específico. 
 *newsize debe almacenar el tamaño del nuevo arreglo que se retorna.
 */
-int *filterEvenNumbers(int arr[], int size, int *newSize) { return NULL; }
+int *filterEvenNumbers(int arr[], int size, int *newSize) {
+  
+  int arrPares[];
+  newSize = 0;
+  
+  for ( int i = 0 ; i < size; i++){
+    if(arr[i] % 2 == 0)
+      arrPares[newSize] = arr[i];
+      newSize++;
+  }
+  return arrPares; 
+}
 
 /*
 Ejercicio 4: Fusión de dos Arreglos Ordenados
